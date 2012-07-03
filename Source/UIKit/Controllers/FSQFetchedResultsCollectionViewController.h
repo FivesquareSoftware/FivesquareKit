91f9094bf257a07b16dc472803dc66902ac90638
John Clayton <john@fivesquare.net>
2012-07-02 18:25:53 -0700

    Adding support for UICollectionView

---
+++ Source/UIKit/Controllers/FSQFetchedResultsCollectionViewController.h
@@ -0,0 +1,30 @@
+//
+//  FSQFetchedResultsCollectionViewController.h
+//  FivesquareKit
+//
+//  Created by John Clayton on 7/2/12.
+//  Copyright (c) 2012 Fivesquare Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface FSQFetchedResultsCollectionViewController : UICollectionViewController <NSFetchedResultsControllerDelegate, UICollectionViewDataSource, UICollectionViewDelegate> {
+	NSManagedObjectContext *_managedObjectContext;
+	NSFetchedResultsController *_fetchedResultsController;
+}
+
+/** @name Properties for subclasses to override
+ *  @{
+ */
+@property (nonatomic, strong) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+
+/** @} */
+
+
+/** Override this in your subclass to configure a cell with a fetched object. */
+- (void)configureCell:(UICollectionViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
+
+
+@end
