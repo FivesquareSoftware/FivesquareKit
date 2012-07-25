e635ddcc03f56f900283ddbc6841d8ae7908937d
John Clayton <john@fivesquare.net>
2012-07-24 18:37:06 -0700

    Fixed warnings

--- Source/Foundation/Categories/NSString+FSQFoundation.m
+++ Source/Foundation/Categories/NSString+FSQFoundation.m
@@ -26,7 +26,7 @@ static NSString *kFSQ_NSStringPathWithOptionalScaleExpression = @"^(\\w+)(@([0-9
 	const char *cStr = [self UTF8String];
 	unsigned char result[CC_MD5_DIGEST_LENGTH];
 	
-	CC_MD5( cStr, strlen(cStr), result );
+	CC_MD5( cStr, (CC_LONG)strlen(cStr), result );
 	
 	NSMutableString *hash = [[NSMutableString alloc] initWithCapacity:33];
 	for (int i = 0; i < 16; i++) {
