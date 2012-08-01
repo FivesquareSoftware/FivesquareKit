28cd4e1bf6ed32817a70ccfeaecf1b287bd9be1c
John Clayton <john@fivesquare.net>
2012-08-01 14:08:50 -0700

    isEmpty for attributed string

---
+++ Source/Foundation/Categories/NSAttributedString+FSQFoundation.h
@@ -0,0 +1,15 @@
+//
+//  NSAttributedString+FSQFoundation.h
+//  FivesquareKit
+//
+//  Created by John Clayton on 8/1/12.
+//  Copyright (c) 2012 Fivesquare Software, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSAttributedString (FSQFoundation)
+
++ (BOOL) isEmpty:(NSAttributedString *)string;
+
+@end
